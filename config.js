11f34ddf0699ec894ded6388664aa7eb4bbe4003
epumav <epumav@pj.gob.pe>
2026-08-19 15:14:54 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// URL pública del backend que consulta Google Calendar.
-// Ejemplo: https://mi-calendario-backend.onrender.com
-window.APP_CONFIG = {
-  apiBaseUrl: "https://TU-BACKEND.example.com",
-  timezone: "America/Lima"
-};
